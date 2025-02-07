116b75ed36fb3a8281fb9733d5154195ad67d1b9
BHARATHV <bharathv0901@gmail.com>
2025-02-07 21:24:31 +0530

    c++

---
+++ print_all_divisor.cpp
@@ -0,0 +1,15 @@
+//Printing all the divisors of given number
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin>>n;
+    cout<<"The divisors of "<<n<<" are:"<<endl;
+    for(int i=1;i<=n;i++){
+        if(n%i==0){
+            cout<<i<<endl;
+        }
+    }
+}
