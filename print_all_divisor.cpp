39113ce631cc608405db1ce29eacd9ecb909d6de
BHARATHV <bharathv0901@gmail.com>
2025-02-07 21:35:43 +0530

    c++

--- print_all_divisor.cpp
+++ print_all_divisor.cpp
@@ -7,9 +7,10 @@ int main(){
     int n;
     cin>>n;
     cout<<"The divisors of "<<n<<" are:"<<endl;
-    for(int i=1;i<=n;i++){
+    for(int i=1;i*i<=n;i++){
         if(n%i==0){
             cout<<i<<endl;
+            cout<<n/i<<endl;
         }
     }
 }
