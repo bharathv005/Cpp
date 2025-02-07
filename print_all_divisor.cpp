1edec4ef3364dbcff07929d23871769fb82dc5c0
BHARATHV <bharathv0901@gmail.com>
2025-02-07 21:50:23 +0530

    c++

--- print_all_divisor.cpp
+++ print_all_divisor.cpp
@@ -6,11 +6,23 @@ using namespace std;
 int main(){
     int n;
     cin>>n;
+    vector<int> d; 
     cout<<"The divisors of "<<n<<" are:"<<endl;
     for(int i=1;i*i<=n;i++){
+
         if(n%i==0){
-            cout<<i<<endl;
-            cout<<n/i<<endl;
+            d.push_back(i);
+
+            if(i!=n/i){
+               d.push_back(n/i);
+            }
         }
+
+            
     }
+        sort(d.begin(),d.end());
+        for(auto j:d){
+            cout<<j<<endl;
+        }
+
 }
