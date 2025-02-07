9eb9a16449a29746f5201a2a18e1b8851b6e0fcd
BHARATHV <bharathv0901@gmail.com>
2025-02-07 21:17:15 +0530

    c++

---
+++ palindrome.cpp
@@ -0,0 +1,22 @@
+//palindrome number
+
+#include<bits/stdc++.h>
+using namespace std;
+int main(){
+    int n,last_digit;
+    int x=0;
+    cin>>n;
+    int m=n;
+    while(n!=0){
+        last_digit=n%10;
+        x=x*10+last_digit;
+        n=n/10;
+    }
+    cout<<x<<endl;
+    if(x==m){
+        cout<<x<<" "<<"is a palindrome number"<<endl;
+    }
+    else{
+        cout<<x<<" "<<"is not a palindrome number"<<endl;
+    }
+}
