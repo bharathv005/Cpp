a554e1d9142e43aff4f9451163a71f82c41463c0
BHARATHV <bharathv0901@gmail.com>
2025-02-07 21:01:33 +0530

    c++

---
+++ reverse_a_number.cpp
@@ -0,0 +1,30 @@
+//Print the digits present in given number
+
+#include <bits/stdc++.h>
+using namespace std;
+int last_digit;
+int x,n;
+int main(){
+    while(n!=0){
+        last_digit=n%10;
+    x=x*10+last_digit;
+    n=n/10;
+    }
+    cout<<x<<endl;
+}
+
+
+
+
+
+// //int main(){
+//     int n;
+//     cin>>n;
+//     int last_digit;
+//     while(n!=0){
+//         last_digit=n%10;
+//         n=n/10;
+//         cout<<last_digit;
+//     }
+    
+// //}
