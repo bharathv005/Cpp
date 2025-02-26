30a63cfa33da16cf7c108127689b65249a09238c
BHARATHV <bharathv0901@gmail.com>
2025-02-26 15:46:58 +0530

    cpp

--- gcd.cpp
+++ gcd.cpp
@@ -1,38 +1,38 @@
 //Find the gcd of given numbers
 
-// #include<bits/stdc++.h>
-// using namespace std;
-// int main(){
-//     int a,b;
-//     cin>>a>>b;
-//     while(a!=0 and b!=0){
-//         if(a>=b){
-//             a=a%b;
-//         }
-//         else{
-//             b=b%a;
-//         }
-//     }
-//     if(a==0){
-//         cout<<b;
-//     }
-//     else{
-//         cout<<a;
-//     }
-// }
-
-
-//Brute force
-
 #include<bits/stdc++.h>
 using namespace std;
 int main(){
-    int a,b,gcd=0;
+    int a,b;
     cin>>a>>b;
-    for(int i=1;i<=a;i++){
-        if(a%i==0 && b%i==0){
-            gcd=i;
+    while(a!=0 and b!=0){
+        if(a>=b){
+            a=a%b;
+        }
+        else{
+            b=b%a;
         }
     }
-    cout<<gcd;
+    if(a==0){
+        cout<<b;
+    }
+    else{
+        cout<<a;
+    }
 }
+
+
+// //Brute force
+
+// #include<bits/stdc++.h>
+// using namespace std;
+// int main(){
+//     int a,b,gcd=0;
+//     cin>>a>>b;
+//     for(int i=1;i<=a;i++){
+//         if(a%i==0 && b%i==0){
+//             gcd=i;
+//         }
+//     }
+//     cout<<gcd;
+// }
