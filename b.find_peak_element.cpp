287d785f118f259916b777bdcab562398efd1801
Bharath V <124847492+bharathv005@users.noreply.github.com>
2025-05-05 20:12:35 +0530

    cpp

--- b.find_peak_element.cpp
+++ b.find_peak_element.cpp
@@ -11,7 +11,7 @@ int findPeakElement(int arr[], int n) {
     int high = n-2;
     while (low <= high) {
         int mid = (low + high) / 2;
-        if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]) {
+        if (arr[mid] > arr[mid - 1] && arr[mid] > arr[mid + 1]){
             return mid;
         }
         if (arr[mid] > arr[mid - 1]) {
